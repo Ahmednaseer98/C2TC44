b6c477c32ddde34965387e06973551d8bf4ff762
mdnas <mdnas@LAPTOP-0ATO9F64>
2022-04-23 06:39:15 +0530

    first commit

---
+++ DAY37/src/DAY37/Application5.java
@@ -0,0 +1,54 @@
+package DAY37;
+
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Application5 extends Frame implements ActionListener	
+{
+	Button b1, b2;
+	TextField t;
+	
+	public Application5()
+	{
+		setLayout(new FlowLayout());
+		
+		add(b1 = new Button(" Click "));
+		add(t = new TextField(20));
+		add(b2 = new Button(" Clear "));
+		
+		// register component buttons with listener
+		b1.addActionListener(this);		
+		b2.addActionListener(this);	
+		
+		addWindowListener(new WindowAdapter() 
+		{
+			@Override
+			public void windowClosing(WindowEvent e)
+			{
+				System.exit(0);
+			}
+		});
+		
+		setSize(300, 300);
+		setVisible(true);
+	}
+	public static void main(String[] args) 
+	{
+		new Application5();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Object o = e.getSource();
+		if(o.equals(b1))
+			t.setText("Hello world");
+		if(o.equals(b2))
+			t.setText(" ");
+	}
+}
